552924f32bac57dc411c09eecc8c603085d10787
GoRbYsKkA <gleb.kovalchuk0609@gmail.com>
2023-02-20 09:49:46 +0200

    completed

--- script.js
+++ script.js
@@ -8,10 +8,5 @@ for (let i = 0; i <= arrLength; i++) {
 
 arr.sort((a, b) => a - b);
 console.log(arr);
-if (arrLength < 1) {
-    console.log('error');
-} else {
-    arr.splice(1, 3);
-    console.log(arr);
-}
 
+(arrLength < 1) ? console.log('error') : arr.splice(1, 3); console.log(arr);
